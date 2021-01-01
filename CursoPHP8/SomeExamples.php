911191928b84fa83c6e3b74c1eef2032ce78c73e
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2021-01-01 18:50:18 +0000

    Finished PHP 8 #010 Variables

--- CursoPHP8/SomeExamples.php
+++ CursoPHP8/SomeExamples.php
@@ -1,5 +1,5 @@
 <?php
-// Lesson 10 Variables
+// PHP 8 #010 Variables
 # They are used to keep data: numbers, texts, etc.
 # We can show and change their value during the execution of the code.
 # They are defined with a dollar symbol ($) and an identifier (name of the variable)
@@ -16,7 +16,24 @@ $variable = "teste";
 $variable01 = "one variable";
 $Variable01 = "a diferente variable";
 
-#Wrong ways to name a variable in PHP
-$1var = 0;
-$!var = 0;
-$variable(1) = 0;
+# Wrong ways to name a variable in PHP
+// $1var = 0;
+// $!var = 0;
+// $variable(1) = 0;
+// $variable one = 0;
+// $variable-one = 0;
+
+# Ways to name a variable in PHP
+$var1 = 0;
+$var_one = 0;
+$varOne = 0;
+$_one = 0; # Not recommended
+$_1 = 0; # Not recommended
+
+# Naming conventions
+$oneVariable = 0; # Camel case
+$one_variable = 0; # With underscores (snake case)
+$OneVariable = 0; # studly case (not recommended)
+
+# The programer should keep the same code style all along the code.
+
