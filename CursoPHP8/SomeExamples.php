1003588e892570763bcff6ce68c62de7beffcc90
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2021-01-01 18:35:27 +0000

    removed index.php from .gitignore .

---
+++ CursoPHP8/SomeExamples.php
@@ -0,0 +1,22 @@
+<?php
+// Lesson 10 Variables
+# They are used to keep data: numbers, texts, etc.
+# We can show and change their value during the execution of the code.
+# They are defined with a dollar symbol ($) and an identifier (name of the variable)
+$variable;
+echo "variable";
+# We can define and initialize a variable with
+$variable = 100;
+echo "variable";
+
+# We can change the type of of the values of a variable 
+$variable = "teste";
+
+# The variables are case sensitive 
+$variable01 = "one variable";
+$Variable01 = "a diferente variable";
+
+#Wrong ways to name a variable in PHP
+$1var = 0;
+$!var = 0;
+$variable(1) = 0;
