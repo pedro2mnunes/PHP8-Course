d66cc649c47ec92ec13c9bb4d91649aff6d548a5
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2021-01-05 00:06:48 +0000

    Combined Op&Incrementing/Dec Op

--- CursoPHP8/SomeExamples.php
+++ CursoPHP8/SomeExamples.php
@@ -52,10 +52,21 @@ $array = [1,2,3]; // array - collection of values
 $person = new Person(); //Object with properties and methods
 $empty = null; // empty variable
 
-# PHP 8 #12 Assign operator and mathematical operators
-$x = 4 + 2; # sum
-$x = 4 - 2; # subtraction
-$x = 4 * 2; # product
-$x = 4 * 2; # division
-$x = 4 % 2; # modulus (remainder of division)
-$x = 4 ** 2; # exponent
+# PHP 8 #12 Assignment operator and mathematical operators
+$x = 4 + 2;  # sum
+$x = 4 - 2;  # subtraction
+$x = 4 * 2;  # product
+$x = 4 * 2;  # division
+$x = a % b;  # modulus (remainder of $a divided by $b)
+$x = a ** b; # exponentiation. Result of raising $a to the power of $b.
+
+# PHP 9  Combined Operators and Incrementing/Decrementing Operators
+$a = 10;
+$a += 20 # 30
+$a -= 3  # 7
+$a *= 2  # 2 
+$a /= 2  # 5
+# ++$a Pre-increment. Increments $a by one, then returns $a.
+# $a++ Post-increment. Returns $a, then increments $a by one.
+# --$a Pre-decrement. Decrements $a by one, then returns $a.
+# $a-- Post-decrement. Returns $a, then decrementes a$ by one.
