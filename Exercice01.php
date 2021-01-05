658ed810b62ed5443fb31f036966ba5964b4be8f
Pedro Miguel Martinho Nunes <44981871+pedro2mnunes@users.noreply.github.com>
2021-01-05 17:49:43 +0000

    Exercices 1 to 4 done.

--- Exercice01.php
+++
@@ -1,3 +0,0 @@
-# 'a' is equal to 1000 and 'b' is equal to half of 'a'
-# Show the value of 'a'
-$a = 1000;
---
+++ Exercise01.php
@@ -0,0 +1,26 @@
+<?php
+
+# 'a' is equal to 1000 and 'b' is equal to half of 'a'
+# Show the value of 'a'
+$a = 1000;
+$b = $a/2;
+echo $b;
+# 'a' is equal to 100 and 'b' is equal to three times the value of 'a' is equal to
+#Show the value of 'b'
+echo '<br>';
+$a = 100;
+$b = 3 * $a;
+echo $b;
+echo'<br>';
+# 'a' is initialized and declared as 20.
+# In the following instruction , 'a' should be presentes with the value 21
+$a = 20;
+echo ++$a;
+echo'<br>';
+#
+$a = 100;
+$b = 250;
+$c = $a + $b;
+echo ++$c;
+
+
